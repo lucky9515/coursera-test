3c2b1522feaf68c35fe3fc7fcc94675ad9642499
Basireddygari <AH16622@ad.wellpoint.com>
2021-03-10 16:39:26 +0530

    new files

---
+++ app.js
@@ -0,0 +1,7 @@
+(function(){
+    'use strict';
+    angular.module('MyFirstApp',[]).
+    controller('firstController',function($scope){
+        $scope.firstName = 'Lakshmi prasanna kumar reddy Venkat are Friends';
+    });
+})();
