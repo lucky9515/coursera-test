f586b1bbdb7dc9e00f18a92660e4f12ab86ecbfc
Basireddygari <AH16622@ad.wellpoint.com>
2021-03-10 19:20:37 +0530

    new folder

--- app.js
+++ app.js
@@ -3,5 +3,25 @@
     angular.module('MyFirstApp',[]).
     controller('firstController',function($scope){
         $scope.firstName = 'Lakshmi prasanna kumar reddy Venkat are Friends';
+        $scope.friendName = "";
+        $scope.countValue = 0;
+        $scope.displayNumeric = function(){
+          var val =  countAsciValue($scope.friendName);
+          $scope.countValue = val;
+
+        };
+
+        function countAsciValue(val){
+            var countVal = 0;
+            for(var i=0;i<val.length;i++){
+
+                countVal += val.charCodeAt(i);
+
+            }
+            return countVal;
+
+        }
+
+
     });
 })();
