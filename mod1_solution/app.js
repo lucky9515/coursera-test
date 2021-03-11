f65ea44236e1de1895b5b601ffb8e7d61b657209
Basireddygari <AH16622@ad.wellpoint.com>
2021-03-11 19:41:15 +0530

    New file

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,8 +1,12 @@
 (function(){
     'use strict';
-    angular.module('LunchCheck',[]).
-    controller('LunchCheckController',function($scope){
-        $scope.items = "";
+angular
+    .module('LunchCheck',[])
+    .controller('LunchCheckController', Controller);
+
+Controller.$inject = ['$scope'];
+function Controller($scope) {
+    $scope.items = "";
         $scope.outputvalue = "";
         $scope.changeOutput = function(){
             if($scope.items===""){
@@ -12,23 +16,17 @@
             var returnvalue = callMethod($scope.items);
 
             $scope.outputvalue = returnvalue;
-
         };
-
+}
         function callMethod(arg){
-
-            var resArray = arg.split(",");
+         var resArray = arg.split(",");
             if(resArray.length<=3){
                 return "Enjoy!";
             }
             else if(resArray.length>3){
                 return "Too much!";
             }
-
         }
-
-        
-
-
-    });
 })();
+
+//!function(){"use strict";function t(t){t.items="",t.outputvalue="",t.changeOutput=function(){if(""!==t.items){var e=function(t){var e=t.split(",");if(e.length<=3)return"Enjoy!";if(e.length>3)return"Too much!"}(t.items);t.outputvalue=e}else t.outputvalue="Please enter data first"}}angular.module("LunchCheck",[]).controller("LunchCheckController",t),t.$inject=["$scope"]}();
